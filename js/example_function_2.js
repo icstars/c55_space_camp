e748a4c6e6507fdd03191d82e46d50cc46672543
rjmagley <ryan.magley@gmail.com>
2024-06-26 16:26:22 -0500

    Set up for continuing sample function

--- js/example_function_2.js
+++ js/example_function_2.js
@@ -5,9 +5,7 @@ function createFullName(first_name, middle_name, last_name) {
     var full_name = "";
 
     full_name += first_name;
-    // we're not quite sure about how to handle the middle name yet -
-    // we know it needs to be present, but we also know that not all people
-    // have a last name. some thought is required here
+    // what can we do here now?
     full_name += ' ' + last_name;
 
     return full_name;
