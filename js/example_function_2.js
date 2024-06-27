bb04a91cb5948d86f34bbc237c26d49827a91099
rjmagley <ryan.magley@gmail.com>
2024-06-26 19:15:39 -0500

    Update test cases

--- js/example_function_2.js
+++ js/example_function_2.js
@@ -14,3 +14,15 @@ function createFullName(first_name, middle_name, last_name) {
 var full_name_result = createFullName("Ryan", "Jacob", "Magley");
 
 console.log(full_name_result);
+
+var full_name_result = createFullName("Alex", "Brian", "Lastname");
+
+console.log(full_name_result);
+
+var full_name_result = createFullName("Charlie", "", "Smith");
+
+console.log(full_name_result);
+
+var full_name_result = createFullName("Devin", null, "Jones");
+
+console.log(full_name_result);
